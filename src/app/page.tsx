d8a18079f0cb476187cfdf4384e1bafeee9eb78e
Shane <70449042+carnealse@users.noreply.github.com>
2025-09-27 16:39:02 -0400

    chore: auto deploy test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get started by editing <code>src/app/page.tsx</code>.
+            Hello World - We Are Just Getting Started
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
